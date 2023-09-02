a1b8cac4ff2b189ed315ecb12c80a6ee8f8def44
Robert Reynolds <rreynold@gmail.com>
2023-09-02 16:00:58 -0400

    Ergoemacs is not failing, but is broken.

--- personal/ergoemacs.el
+++ personal/ergoemacs.el
@@ -1,10 +1,10 @@
+(prelude-require-package 'ergoemacs-mode)
 
- ;; (require 'ergoemacs-mode)
- ;; (ergoemacs-ignore-prev-global) ; Do not honor previously defined
- ;;                                ; global keys.
-  ;;(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
-  ;;(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
-  ;;(ergoemacs-mode 1)
+(require 'ergoemacs-mode)
+;;(ergoemacs-ignore-prev-global) ; Do not honor previously defined global keys.
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+(ergoemacs-mode 1)
 
   ;; TODO: Figure out how to remap ctrl+z to undo. Currently mapped to alt-z in ergoemacs.
   (with-eval-after-load 'undo-tree (defun undo-tree-overridden-undo-bindings-p () nil))
